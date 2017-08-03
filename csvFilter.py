827558599a756a0d896cd15dc54bdeaced3a198b
Jonathan Schultz <jonathan@schultz.la>
2017-08-03 17:26:53 +1000

    Updated copyright year to 2017

--- csvFilter.py
+++ csvFilter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2016 Jonathan Schultz
+# Copyright 2017 Jonathan Schultz
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
